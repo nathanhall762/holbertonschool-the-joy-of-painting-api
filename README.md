96feb638eb7a3f281d28ebb489835763386dada9
nathanhall762 <nathanhall762@gmail.com>
2023-08-02 15:03:53 -0500

    api implemented!

--- README.md
+++ README.md
@@ -1,5 +1,54 @@
-# holbertonschool-the-joy-of-painting-api
+# The Joy of Painting API
+
 ![Database UML](https://github.com/nathanhall762/holbertonschool-the-joy-of-painting-api/blob/main/DatabaseUML.jpeg)
 
-mysql username: root
-mysql password: new_password
+This project is a RESTful API built with Node.js and MySQL that allows users to search for episodes of Bob Ross's show "The Joy of Painting" based on different criteria such as month, subject, color, and match type.
+
+## Requirements
+
+- Node.js
+- MySQL
+
+## Setup
+
+Clone the repository:
+
+```
+git clone https://github.com/nathanhall762/holbertonschool-the-joy-of-painting-api.git
+```
+
+Navigate into the directory:
+```
+cd holbertonschool-the-joy-of-painting-api
+```
+
+Install the dependencies:
+```
+npm install
+```
+
+## Starting the Server
+
+You can start the server by running:
+```
+npm start
+```
+
+The server will start on port 3000 by default. You can access the API at http://localhost:3000.
+API Endpoints
+
+GET /api/episodes/search: This endpoint allows users to search for episodes based on different criteria. 
+The parameters for this endpoint are as follows:
+- month: Filters the episodes based on the month they were aired.
+- subject: Filters the episodes based on the subjects painted in the episode.
+- color: Filters the episodes based on the colors used in the episode.
+- matchType: Determines how the filters are applied.
+ If set to ALL, the API will return episodes that match all of the selected filters. If set to anything else, the API will return episodes that match one or more of the selected filters.
+
+## Testing
+
+To test the API, you can use curl or any API testing tool such as Postman. Here's an example using curl:
+
+```
+curl "http://localhost:3000/api/episodes/search?month=January&subject=Tree&color
+```
--- index.js
+++ index.js
@@ -1,35 +1,93 @@
-const { checkDatabaseExists, createDBConnectionWithoutDB } = require('./src/checkDatabase');
-const fs = require('fs');
-const path = require('path');
-const loadDataIntoDatabase = require('./src/loadDataIntoDatabase');
-const createDBConnection = require('./src/dbConnection');
+const {
+  checkDatabaseExists,
+  createDBConnectionWithoutDB,
+} = require("./src/checkDatabase");
+const fs = require("fs");
+const path = require("path");
+const loadDataIntoDatabase = require("./src/loadDataIntoDatabase");
+const createDBConnection = require("./src/dbConnection");
+const express = require("express");
 
-checkDatabaseExists().then(exists => {
+checkDatabaseExists()
+  .then((exists) => {
     if (!exists) {
-        // Read the SQL file
-        const createDatabaseSQL = fs.readFileSync(path.join(__dirname, './sql/createDatabase.sql'), 'utf-8');
+      // Read the SQL file
+      const createDatabaseSQL = fs.readFileSync(
+        path.join(__dirname, "./sql/createDatabase.sql"),
+        "utf-8"
+      );
 
-        // Create a connection
-        const db = createDBConnectionWithoutDB();
+      // Create a connection
+      const db = createDBConnectionWithoutDB();
 
-        // Create the database
-        db.query(createDatabaseSQL, (error, results, fields) => {
-            if (error) {
-                console.log('An error occurred while creating the database: ', error);
-                db.end(); // Close the connection if there's an error
-                return;
-            }
+      // Create the database
+      db.query(createDatabaseSQL, (error, results, fields) => {
+        if (error) {
+          console.log("An error occurred while creating the database: ", error);
+          db.end(); // Close the connection if there's an error
+          return;
+        }
 
-            console.log('Database and tables created successfully.');
-            db.end(); // Close the connection when done
-        });
+        console.log("Database and tables created successfully.");
+        db.end(); // Close the connection when done
+      });
+
+      // If database was just created or already existed, load data into it
+      loadDataIntoDatabase();
+
     } else {
-        console.log("Database already exists.");
+      console.log("Database already exists.");
     }
-}).then(() => {
-    // Regardless if database was just created or already existed, load data into it
-    loadDataIntoDatabase();
-})
-.catch(err => {
-    console.log('An error occurred: ', err);
-});
+  })
+  .then(() => {
+    const app = express();
+    const port = 3000;
+
+    // Setup the database connection
+    const connection = createDBConnection();
+
+    app.get("/api/episodes/search", async (req, res) => {
+      const { month, subject, color, matchType } = req.query;
+
+      let filters = [];
+      if (month) filters.push(`month = ${connection.escape(month)}`);
+      if (subject) filters.push(`s.subject_name = ${connection.escape(subject)}`);
+      if (color) filters.push(`c.color_name = ${connection.escape(color)}`);
+  
+
+      let filterString = filters.join(
+        matchType.toUpperCase() === "ALL" ? " AND " : " OR "
+      );
+
+      // Construct the SQL query with necessary join and grouping conditions
+      let sql = `
+      SELECT e.episode_title
+      FROM Episodes e 
+      LEFT JOIN Episode_Subject es ON e.episode_id = es.Episode_Id
+      LEFT JOIN Subjects s ON es.Subject_Id = s.Subject_Id
+      LEFT JOIN Episode_Color ec ON e.episode_id = ec.Episode_Id
+      LEFT JOIN Colors c ON ec.Color_Id = c.Color_Id
+    `;
+
+      // If filters exist, append WHERE clause
+      if (filterString) sql += `WHERE ${filterString}`;
+
+      connection.query(sql, (error, results) => {
+        if (error) throw error;
+        res.json(results);
+      });
+    });
+
+    app.listen(port, () => {
+      console.log(`API is running at http://localhost:${port}`);
+    });
+
+    // Close connection when app is shutting down
+    process.on("SIGINT", () => {
+      connection.end();
+      process.exit();
+    });
+  })
+  .catch((err) => {
+    console.log("An error occurred: ", err);
+  });
